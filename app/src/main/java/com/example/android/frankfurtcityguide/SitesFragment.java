66e7ef81581dba247a4632ead565513da1543c78
Diederik de Neef <diederik.deneef@gmail.com>
2020-07-21 11:15:16 +0200

    feat: Add RecyclerView to Sites Fragment

--- app/src/main/java/com/example/android/frankfurtcityguide/SitesFragment.java
+++ app/src/main/java/com/example/android/frankfurtcityguide/SitesFragment.java
@@ -3,6 +3,8 @@ package com.example.android.frankfurtcityguide;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +12,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SitesFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class SitesFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +61,25 @@ public class SitesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sites, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sites, container, false);
+
+        String[] data = new String[50];
+        for (int i = 1; i < 50; i++) {
+            data[i] = "bladiebla " + i;
+        }
+
+        // Create the recycler view
+        recyclerView = rootView.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+
+        // Set a layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(rootView.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Set the adapter
+        rvAdapter adapter = new rvAdapter(getContext(),data);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/example/android/frankfurtcityguide/rvAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.frankfurtcityguide;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class rvAdapter extends RecyclerView.Adapter<rvAdapter.ViewHolder> {
+
+    // Variables
+    private String[] mData;
+    private LayoutInflater mInflater;
+
+    // Constructor
+    rvAdapter(Context context, String[] data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // Inflate the recycler row item
+    @NonNull
+    @Override
+    public rvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycler_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Bind data to the TextView in each recycler row item
+    @Override
+    public void onBindViewHolder(@NonNull rvAdapter.ViewHolder holder, int position) {
+        String randomText = mData[position];
+        holder.germanDescription.setText(randomText);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView germanDescription;
+//        TextView englishDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            germanDescription = itemView.findViewById(R.id.german_description);
+        }
+    }
+}
