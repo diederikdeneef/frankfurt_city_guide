90ebfbf44a9e4715dcd7f96896e838c6901e463d
Diederik de Neef <diederik.deneef@gmail.com>
2020-07-29 10:06:52 +0200

    feat: Add up button to text credits screen

--- app/src/main/java/com/example/android/frankfurtcityguide/TextCreditsActivity.java
+++ app/src/main/java/com/example/android/frankfurtcityguide/TextCreditsActivity.java
@@ -14,6 +14,7 @@ public class TextCreditsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text_credits);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView textCreditsView = (TextView) findViewById(R.id.text_credits);
         textCreditsView.setText(R.string.text_credits);
